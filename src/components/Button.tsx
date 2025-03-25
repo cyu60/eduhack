e1eeb16e8759a23da00a5d44828f88020460a592
AureliaSindhu <aurelia.sindhu@gmail.com>
2025-03-24 22:05:24 -0700

    Revert "nav"
    This reverts commit 1e761b23b39988bb329a1990d643ea9dd8d3e657.

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,6 +1,5 @@
 import Link from 'next/link'
 import clsx from 'clsx'
-import '@/app/global.css'
 
 type ButtonProps =
   | React.ComponentPropsWithoutRef<typeof Link>
@@ -8,7 +7,7 @@ type ButtonProps =
 
 export function Button({ className, ...props }: ButtonProps) {
   className = clsx(
-    'inline-flex justify-center rounded-2xl bg-[var(--darkRed)] pt-2 pb-2 pl-4 pr-4 text-base font-semibold text-white hover:bg-[var(--red)] focus:outline-hidden focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-500 active:text-white/70',
+    'inline-flex justify-center rounded-2xl bg-blue-600 p-4 text-base font-semibold text-white hover:bg-blue-500 focus:outline-hidden focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-500 active:text-white/70',
     className,
   )
 
