a43b4ad40ccdd6b2f1852f6d8cb919c57a0e80f8
AureliaSindhu <aurelia.sindhu@gmail.com>
2025-03-24 20:54:00 -0700

    testing

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,6 @@
 import { Button } from '@/components/Button'
 import { Container } from '@/components/Container'
 import { DiamondIcon } from '@/components/DiamondIcon'
-// import { Logo } from '@/components/Logo'
 import Image from 'next/image'
 import  headerLogo  from "@/public/headerLogo.png"
 
@@ -10,9 +9,7 @@ export function Header() {
     <header className="relative z-50 flex-none lg:pt-11">
       <Container className="flex flex-wrap items-center justify-center sm:justify-between lg:flex-nowrap">
         <div className="flex items-center mt-10 lg:mt-0 lg:grow lg:basis-0">
-          {/* <Logo className="h-12 w-auto text-slate-900" /> */}
           <Image src={headerLogo} alt="EduHack" width={180} height={32} />
-          {/* <h1 className="ml-2 text-3xl font-bold text-[var(--navy)]">Edu<span className="italic">Hack</span></h1> */}
         </div>
         <div className="order-first -mx-4 flex flex-auto basis-full overflow-x-auto border-b border-blue-600/10 py-4 font-mono text-sm whitespace-nowrap text-blue-600 sm:-mx-6 lg:order-none lg:mx-0 lg:basis-auto lg:border-0 lg:py-0">
           <div className="mx-auto flex items-center gap-4 px-4">
