88b5e6ffabf7ad928351d031f123b92150258bed
cyu60 <chinatchinat123@gmail.com>
2025-04-08 13:32:04 -0700

    feat: hide button

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,12 @@
 import { Button } from '@/components/Button'
 import { Container } from '@/components/Container'
 import Image from 'next/image'
-import headerLogo from "@/public/headerLogoWhite.png"
+import headerLogo from '@/public/headerLogoWhite.png'
 import '@/app/global.css'
 
 export function Header() {
   return (
-    <header className="relative z-50 flex-none py-4 bg-[var(--darkRed)]">
+    <header className="relative z-50 flex-none bg-[var(--darkRed)] py-4">
       <Container className="flex flex-wrap items-center justify-center sm:justify-between lg:flex-nowrap">
         <div className="flex items-center lg:mt-0 lg:grow lg:basis-0">
           <Image src={headerLogo} alt="EduHack" width={180} height={32} />
@@ -22,7 +22,7 @@ export function Header() {
           </div>
         </div>
         <div className="hidden sm:mt-10 sm:flex lg:mt-0 lg:grow lg:basis-0 lg:justify-end">
-          <Button variant="cream" href="#" className="bg-[var(--cream)]">Register Now</Button>
+          {/* <Button variant="cream" href="#" className="bg-[var(--cream)]">Register Now</Button> */}
         </div>
       </Container>
     </header>
--- src/components/Sponsors.tsx
+++ src/components/Sponsors.tsx
@@ -26,7 +26,7 @@ export function Sponsors() {
         {/* Partners Section */}
         <div className="mb-24">
           <h2 className="mx-auto max-w-2xl text-center font-display text-4xl font-medium tracking-tighter text-[var(--darkRed)] sm:text-5xl">
-            In Partnership With
+            Organized in Partnership With
           </h2>
           <div className="mx-auto mt-12 grid max-w-max grid-cols-1 place-content-center gap-x-32 gap-y-16">
             {partners.map((partner) => (
