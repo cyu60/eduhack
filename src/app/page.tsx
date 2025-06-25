d215f8195fe8dfb3f7eb34174cdb809387a328ec
AureliaSindhu <aurelia.sindhu@gmail.com>
2025-06-24 22:27:12 -0700

    refactor: directory

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,23 @@
-import { Hero } from '@/components/sum25/Hero'
-import { Newsletter } from '@/components/sum25/Newsletter'
+// import { Hero } from '@/components/sum25/Hero'
+// import { Newsletter } from '@/components/sum25/Newsletter'
 // import { Schedule } from '@/components/Schedule'
 // import { UpcomingSchedule } from '@/components/sum25/UpcomingSchedule'
-import Location from '@/components/sum25/Location'
-import { Speakers } from '@/components/sum25/Speakers'
-import { Sponsors } from '@/components/sum25/Sponsors'
-import { Schedule } from '@/components/sum25/Schedule'
+// import Location from '@/components/sum25/Location'
+// import { Speakers } from '@/components/sum25/Speakers'
+// import { Sponsors } from '@/components/sum25/Sponsors'
+// import { Schedule } from '@/components/sum25/Schedule'
+import { redirect } from 'next/navigation'
 
 export default function Home() {
-  return (
-    <>
-      <Hero />
-      <Location />
-      <Schedule />
-      <Speakers />
-      <Sponsors />
-      {/* <Newsletter /> */}
-    </>
-  )
+  redirect('/in-person')
+  // return (
+  //   <>
+  //     <Hero />
+  //     <Location />
+  //     <Schedule />
+  //     <Speakers />
+  //     <Sponsors />
+  //     {/* <Newsletter /> */}
+  //   </>
+  // )
 }
